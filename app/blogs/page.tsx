aaedb0a250c5cbd4eb009d4749c6fc4a2338367e
Nitish Bhardwaj <itsnitish9319192299@gmail.com>
2026-09-17 14:13:43 +0530

    Add footer to blogs listing page and scroll arrow to bottom-right

--- app/blogs/page.tsx
+++ app/blogs/page.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { getWordPressPosts } from "@/lib/getPosts";
 import BlogList from "@/components/blog/BlogList";
+import Footer from "@/components/layout/Footer";
 import { buildMetadata } from "@/lib/seo";
 
 export const metadata: Metadata = buildMetadata({
@@ -16,6 +17,7 @@ export default async function AllBlogsPage() {
   return (
     <main className="bg-[#004dc3] min-h-screen">
       <BlogList posts={posts} />
+      <Footer />
     </main>
   );
 }
